1ddd826c37f1eb4a55726e9d90f847cacbc785c4
Laura <lgomez25@ucm.es>
2023-02-21 12:32:01 +0100

    ogre.bat
    Co-Authored-By: Sergio <82314571+Sebana02@users.noreply.github.com>

---
+++ Dependencias/Ogre/build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set EXES=.\..\..\Exes
+set CMAKE=.\..\..\CMake\bin\cmake.exe
+set SRC=..\Src
+
+mkdir .\build
+cd .\build
+
+%CMAKE% -DOGRE_BUILD_COMPONENT_BITES:BOOL="0" -DOGRE_BUILD_COMPONENT_BULLET:BOOL="0" -DOGRE_BUILD_RENDERSYSTEM_GL:BOOL="0" -DOGRE_INSTALL_DOCS:BOOL="0" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_SAMPLES:BOOL="0" -DOGRE_INSTALL_SAMPLES:BOOL="0" %SRC%
+
+msbuild "OGRE.sln" /p:configuration=Debug
+msbuild "OGRE.sln" /p:configuration=Release
+
+cd ..
+
+XCOPY /y /s .\build\bin\debug\Codec_STBI_d.dll %EXES%
+XCOPY /y /s .\build\bin\debug\RenderSystem_GL3Plus_d.dll %EXES%
+XCOPY /y /s .\build\bin\debug\OgreMain_d.dll %EXES%
+XCOPY /y /s .\build\bin\debug\zlib.dll %EXES%
+
+XCOPY /y /s .\build\bin\release\Codec_STBI.dll %EXES%
+XCOPY /y /s .\build\bin\release\RenderSystem_GL3Plus.dll %EXES%
+XCOPY /y /s .\build\bin\release\OgreMain.dll %EXES%
+
