4db4a329d11c0f01a192580a67c86027e52c7092
Laura <lgomez25@ucm.es>
2023-02-21 11:00:34 +0100

    .bat fmod
    Co-Authored-By: Sergio <82314571+Sebana02@users.noreply.github.com>

---
+++ Dependencias/FMOD/build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set EXES=.\..\..\Exes
+
+XCOPY /y /s .\Src\lib\fmod.dll %EXES%
+XCOPY /y /s .\Src\lib\fmodL.dll %EXES%
+
+XCOPY /y /s .\Src\lib\fmodstudio.dll %EXES%
+XCOPY /y /s .\Src\lib\fmodstudioL.dll %EXES%
+
+XCOPY /y /s .\Src\lib\fsbank.dll %EXES%
+XCOPY /y /s .\Src\lib\flibfsbvorbis.dll %EXES%
+XCOPY /y /s .\Src\lib\opus.dll %EXES%
