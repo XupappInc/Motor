73eaade1f80a3a8a54dbefd164b1b1d855ed981e
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2023-02-22 20:22:06 +0100

    Creados component y entity
    falta ajustar cosas del component (active, destruccion que avise a manager...) y documentar cada método en español

---
+++ EntityComponent/ec_defs.h
@@ -0,0 +1,24 @@
+// Note that the backslash \ after each line below is very important
+// when using #define, if you remove it them put all at the same
+// line of #define
+//
+
+#pragma once
+
+// Components list - must have at least one element
+//
+#define _CMPS_LIST_ \ 
+		_EXAMPLE1, \ 
+		_EXAMPLE2
+
+// Groups list - must have at least one element
+//
+#define _GRPS_LIST_ \
+		_EXAMPLE1_GRP_, \
+		_EXAMPLE2_GRP
+
+// Handlers list - must have at least one element
+//
+#define _HDLRS_LIST_ \
+		_HDLR_EXAMPLE1, \
+		_HDLR_EXAMPLE2
