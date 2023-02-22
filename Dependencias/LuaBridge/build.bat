809d1b7cb46607f650d374f49fa17b7a2d8f3690
Sebana02 <sebana@ucm.es>
2023-02-22 11:48:17 +0100

    .bat para lua y opciones del bat de ogre

---
+++ Dependencias/LuaBridge/build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set EXES=.\..\..\Exes
+set CMAKE=.\..\..\CMake\bin\cmake.exe
+set SRC=..\Src
+
+mkdir .\build
+cd .\build
+
+%CMAKE% -DLUABRIDGE_TESTING:BOOL="0" -Dgtest_force_shared_crt:BOOL="0" -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release;" -DLUABRIDGE_CXX17:BOOL="1" %SRC%
+
+msbuild "LuaBridge.sln" /p:configuration=Debug
+msbuild "LuaBridge.sln" /p:configuration=Release
+pause
+cd ..
