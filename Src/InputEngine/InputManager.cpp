888d1f9162941dfac99219e4c9ceba6fbda2dea3
Vicnuel5 <81177633+Vicnuel5@users.noreply.github.com>
2023-02-25 21:24:15 +0100

    avances con el input manager

--- Src/InputEngine/InputManager.cpp
+++ Src/InputEngine/InputManager.cpp
@@ -1,9 +1,129 @@
 #include "InputManager.h"
 
-#include <SDL_main.h>
-#include <SDL.h>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[]) { 
+InputManager::InputManager() : event() {
+	kbState_ = SDL_GetKeyboardState(0);
+	clearState();
+}
+
+InputManager::~InputManager() {}
+
+inline void InputManager::update() {
+	clearState();
+	while(SDL_PollEvent(&event)) {
+		switch(event.type) {
+			case SDL_KEYDOWN:
+				onKeyDown();
+				break;
+			case SDL_KEYUP:
+				onKeyUp();
+				break;
+			case SDL_MOUSEMOTION:
+				onMouseMotion();
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				onMouseButtonChange(DOWN);
+				break;
+			case SDL_MOUSEBUTTONUP:
+				onMouseButtonChange(RELEASED);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+inline bool InputManager::keyDownEvent() { 
+	return isKeyDownEvent_; 
+}
+
+inline bool InputManager::keyUpEvent() { 
+	return isKeyUpEvent_; 
+}
+
+inline bool InputManager::isKeyDown(char key) {
+	return isKeyDown(SDL_GetScancodeFromKey(key));
+}
+
+inline bool InputManager::isKeyUp(char key) {
+	return isKeyUp(SDL_GetScancodeFromKey(key));
+}
+
+inline bool InputManager::mouseMotionEvent() { 
+	return isMouseMotionEvent_; 
+}
+
+inline bool InputManager::mouseButtonEvent() { 
+	return isMouseButtonEvent_; 
+}
+
+inline bool InputManager::isMouseButtonDown(MOUSEBUTTON b) {
+	return mbState_[b] == DOWN;
+}
+
+inline bool InputManager::isMouseButtonHeld(MOUSEBUTTON b) {
+	return mbState_[b] == DOWN || mbState_[b] == HELD;
+}
+
+inline bool InputManager::isMouseButtonUp(MOUSEBUTTON b) {
+	return mbState_[b] == RELEASED;
+}
+
+inline void InputManager::clearState() {
+	isKeyDownEvent_ = false;
+	isKeyUpEvent_ = false;
+	isMouseButtonEvent_ = false;
+	isMouseMotionEvent_ = false;
+	for(auto i = 0u; i < 3; i++) {
+		if(mbState_[i] == DOWN)
+			mbState_[i] = HELD;
+		else if(mbState_[i] == RELEASED)
+			mbState_[i] = a;
+	}
+}
+
+inline void InputManager::onKeyDown() { 
+	isKeyDownEvent_ = true; 
+}
+
+inline void InputManager::onKeyUp() { 
+	isKeyUpEvent_ = true; 
+}
+
+inline bool InputManager::isKeyDown(SDL_Scancode key) {
+	return keyDownEvent() && kbState_[key] == 1;
+}
+
+inline bool InputManager::isKeyUp(SDL_Scancode key) {
+	return keyUpEvent() && kbState_[key] == 0;
+}
+
+inline void InputManager::onMouseMotion() {
+	isMouseMotionEvent_ = true;
+	mousePos_.first = event.motion.x;
+	mousePos_.second = event.motion.y;
+}
+
+inline void InputManager::onMouseButtonChange(MOUSESTATE mouseState) {
+	isMouseButtonEvent_ = true;
+	switch(event.button.button) {
+		case SDL_BUTTON_LEFT:
+			mbState_[LEFT] = mouseState;
+			break;
+		case SDL_BUTTON_MIDDLE:
+			mbState_[MIDDLE] = mouseState;
+			break;
+		case SDL_BUTTON_RIGHT:
+			mbState_[RIGHT] = mouseState;
+			break;
+		default:
+			break;
+	}
+}
+
+int main(int argc, char* argv[]) {
 	// Inicializar SDL
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -12,18 +132,26 @@ int main(int argc, char* argv[]) {
 	    SDL_CreateWindow("Ejemplo de Input con SDL", SDL_WINDOWPOS_UNDEFINED,
 	                     SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
 
-	// Crear un evento
-	SDL_Event event;
+	InputManager* inputManger = new InputManager();
 
 	// Bucle principal
 	bool quit = false;
 	while(!quit) {
-		// Procesar eventos
-		while(SDL_PollEvent(&event) != 0) {
-			// Si se presiona la tecla ESC, salir del bucle
-			if(event.type == SDL_KEYDOWN &&
-			   event.key.keysym.sym == SDLK_ESCAPE) {
-				quit = true;
+		inputManger->update();
+		if(inputManger->isKeyDown('q')) {
+			quit = true;
+		} else {
+			if(inputManger->isKeyDown('w')) {
+				cout << "Tecla w Pulsada\n";
+			}
+			if(inputManger->isMouseButtonDown(InputManager::LEFT)) {
+				cout << "Click\n";
+			}
+			if(inputManger->isMouseButtonHeld(InputManager::LEFT)) {
+				cout << "Hold\n";
+			}
+			if(inputManger->isMouseButtonUp(InputManager::LEFT)) {
+				cout << "Release\n";
 			}
 		}
 	}
--- Src/InputEngine/InputManager.h
+++ Src/InputEngine/InputManager.h
@@ -3,7 +3,77 @@
 #define __INPUT_MANAGER_H__
 
 //#include "EntityComponent/ManagerTemplate"
+
+#include <SDL.h>
+#include <array>
+
 class InputManager {
 
+public:
+	enum MOUSEBUTTON : uint8_t { LEFT = 0, MIDDLE = 1, RIGHT = 2 };
+
+	InputManager();
+	virtual ~InputManager();
+
+	// update
+	inline void update();
+
+	// keyboard
+	inline bool keyDownEvent();
+
+	inline bool keyUpEvent();
+
+	inline bool isKeyDown(char key);
+
+	inline bool isKeyUp(char key);
+
+	// mouse
+	inline bool mouseMotionEvent();
+
+	inline bool mouseButtonEvent();
+
+	inline const std::pair<Sint32, Sint32>& getMousePos() { return mousePos_; }
+
+	inline bool isMouseButtonDown(MOUSEBUTTON b);
+
+	inline bool isMouseButtonHeld(MOUSEBUTTON b);
+
+	inline bool isMouseButtonUp(MOUSEBUTTON b);
+
+	// TODO add support for Joystick, see Chapter 4 of
+	// the book 'SDL Game Development'
+
+private:
+	enum MOUSESTATE : uint8_t {
+		a = 0,
+		DOWN = 1,
+		HELD = 2,
+		RELEASED = 3,		
+	};
+	
+	// clear the state
+	inline void clearState();
+
+	inline void onKeyDown();
+
+	inline void onKeyUp();
+
+	inline bool isKeyDown(SDL_Scancode key);
+
+	inline bool isKeyUp(SDL_Scancode key);
+
+	inline void onMouseMotion();
+
+	inline void onMouseButtonChange(MOUSESTATE mouseState);
+
+	bool isKeyUpEvent_;
+	bool isKeyDownEvent_;
+	bool isMouseMotionEvent_;
+	bool isMouseButtonEvent_;
+	std::pair<Sint32, Sint32> mousePos_;
+	std::array<uint8_t, 3> mbState_;
+	const Uint8* kbState_;
+
+	SDL_Event event;
 };
 #endif  __INPUT_MANAGER_H__
