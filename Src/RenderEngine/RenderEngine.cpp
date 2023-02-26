3c169d9374cf4fce0e2d6444a133025b2c6ce6f6
PaulaMorillasAlonso <pamorill@ucm.es>
2023-02-26 17:47:13 +0100

    ventana render que se puede cerrar

--- Src/RenderEngine/RenderEngine.cpp
+++ Src/RenderEngine/RenderEngine.cpp
@@ -1,21 +1,36 @@
-// RenderSystem.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// RenderSystem.cpp : Este archivo contiene la función "main". La ejecución del
+// programa comienza y termina ahí.
 //
 
-#include <iostream>
 #include <OgreRoot.h>
+#include <SDL.h>
+using namespace Ogre;
 
-int main()
-{
-	Ogre::Root* root;
-	root = new Ogre::Root();
+int main(int argc, char* argv[]) {
+
+	// Inicializar SDL
+	if(!SDL_WasInit(SDL_INIT_VIDEO))
+		SDL_InitSubSystem(SDL_INIT_VIDEO);
+
+	int screenW=1024, screenH=768;
+	// Crear una ventana
+	SDL_Window* window = SDL_CreateWindow("Separity", SDL_WINDOWPOS_UNDEFINED,
+	                     SDL_WINDOWPOS_UNDEFINED, screenW, screenH, SDL_WINDOW_SHOWN);
+	// Bucle principal
+	SDL_Event event;
+	bool quit = false;
+	while(!quit) {
+		while(SDL_PollEvent(&event)) {
+			switch(event.type) {
+				case SDL_QUIT:
+					quit = true;
+					break;
+			}
+		}
+	}
+	// Liberar recursos y cerrar SDL
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
