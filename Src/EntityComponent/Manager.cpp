ed194aece2158b5016042dd1595fb6d967274bc3
Laura Gomez <lgomez25@ucm.es>
2023-02-28 21:01:06 +0100

    refactorizacion clase manager

--- Src/EntityComponent/Manager.cpp
+++ Src/EntityComponent/Manager.cpp
@@ -1,23 +1,23 @@
 #include "Manager.h"
-
+#include "Component.h"
 #include <cassert>
 
-void Manager::update() {
-	//for(Component* c : cmps_) { c->update(); }
+void ec::Manager::update() {
+	for(ec::Component* c : cmps_) { c->update(); }
 }
 
-void Manager::addComponent(Component* cmp) {
+void ec::Manager::addComponent(ec::Component* cmp) {
 	assert(cmp != nullptr);
 	cmps_.push_back(cmp);
 }
 
-void Manager::removeComponent(Component* cmp) {
+void ec::Manager::removeComponent(ec::Component* cmp) {
 	assert(cmp != nullptr);
 	auto iter = std::find(cmps_.begin(), cmps_.end(), cmp);
 
 	cmps_.erase(iter);
 }
 
-Manager::Manager() {}
+ec::Manager::Manager() {}
 
-Manager::~Manager() {}
+ec::Manager::~Manager() {}
