daf64cf2ebe73e82bdb188b9d42c74cdb7327a96
jacall02 <82378764+jacall02@users.noreply.github.com>
2023-03-05 12:10:04 +0100

    PhysicsManager documentado
    Co-Authored-By: MIRIAM MARTIN <81752837+miriam-m-s@users.noreply.github.com>

--- Src/PhysicsEngine/PhysicsManager.cpp
+++ Src/PhysicsEngine/PhysicsManager.cpp
@@ -1,5 +1,5 @@
 #include "PhysicsManager.h"
-//#include "checkML.h"
+// #include "checkML.h"
 
 #include <btBulletDynamicsCommon.h>
 template<typename T>
@@ -10,18 +10,32 @@ PhysicsManager::PhysicsManager() {}
 PhysicsManager::~PhysicsManager() {}
 
 void PhysicsManager::initWorld() {
-	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
-	btDefaultCollisionConfiguration* collisionConfiguration =
-	    new btDefaultCollisionConfiguration();
-	btCollisionDispatcher* dispatcher =
-	    new btCollisionDispatcher(collisionConfiguration);
-	btSequentialImpulseConstraintSolver* solver =
-	    new btSequentialImpulseConstraintSolver;
-	world_ = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver,
-	                                     collisionConfiguration);
+	broadphase_ = new btDbvtBroadphase();
+	collisionConfiguration_ = new btDefaultCollisionConfiguration();
+	dispatcher_ = new btCollisionDispatcher(collisionConfiguration_);
+	solver_ = new btSequentialImpulseConstraintSolver;
+	world_ = new btDiscreteDynamicsWorld(dispatcher_, broadphase_, solver_,
+	                                     collisionConfiguration_);
 }
 
-void PhysicsManager::update() { world_->stepSimulation(btScalar(0.02)); }
+void Separity::PhysicsManager::deleteWorld() {
+	for(int i = world_->getNumCollisionObjects() - 1; i >= 0; i--) {
+		btCollisionObject* obj = world_->getCollisionObjectArray()[i];
+		btRigidBody* body = btRigidBody::upcast(obj);
+		if(body && body->getMotionState()) {
+			delete body->getMotionState();
+		}
+		world_->removeCollisionObject(obj);
+		delete obj;
+	}
+	delete broadphase_;
+	delete collisionConfiguration_;
+	delete dispatcher_;
+	delete solver_;
+	delete world_;
+}
+
+void PhysicsManager::update() { world_->stepSimulation(1.0 / 60.0, 10); }
 
 btDiscreteDynamicsWorld* PhysicsManager::getWorld() { return world_; }
 
--- Src/PhysicsEngine/PhysicsManager.h
+++ Src/PhysicsEngine/PhysicsManager.h
@@ -8,18 +8,47 @@ namespace Separity {
 	class PhysicsManager : public Separity::Manager,
 	                       public Singleton<Separity::PhysicsManager> {
 		friend Singleton<PhysicsManager>;
+
 		public:
+		/// <summary>
+		/// Crea un mundo dinámico discreto de bullet
+		/// </summary>
 		void initWorld();
+		/// <summary>
+		/// Borra el mundo de bullet y los objetos de este
+		/// </summary>
+		void deleteWorld();
+		/// <summary>
+		/// Método update que actualiza la simulación física del mundo
+		/// </summary>
 		virtual void update() override;
+		/// <summary>
+		///	Obtiene un puntero del mundo de bullet
+		/// </summary>
+		/// <returns>El mundo donde sucede la simulación</returns>
 		btDiscreteDynamicsWorld* getWorld();
+		/// <summary>
+		/// Obtiene la instancia del Manager de físicas
+		/// </summary>
+		/// <returns>La instancia del Manager</returns>
 		static PhysicsManager* getInstance();
+		/// <summary>
+		/// El destructor heredado del Manager
+		/// </summary>
 		virtual ~PhysicsManager() override;
 
 		private:
+		/// <summary>
+		/// Constructor de la clase
+		/// </summary>
 		inline PhysicsManager();
-		
+
 		btDiscreteDynamicsWorld* world_;
+		btBroadphaseInterface* broadphase_;
+		btCollisionDispatcher* dispatcher_;
+		btSequentialImpulseConstraintSolver* solver_;
+		btDefaultCollisionConfiguration* collisionConfiguration_;
 	};
-}
+}  // namespace Separity
 
 #endif __PHYSICS_MANAGER_H__
