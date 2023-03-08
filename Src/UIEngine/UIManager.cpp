5c98a1c4b5c446c8f7127272bd8eaf012670cb54
Laura <lgomez25@ucm.es>
2023-03-08 11:10:24 +0100

    Base del UI manager

--- Src/UIEngine/UIManager.cpp
+++ Src/UIEngine/UIManager.cpp
@@ -2,3 +2,7 @@
 
 template<typename T>
 std::unique_ptr<T> Singleton<T>::_INSTANCE_;
+
+Separity::UIManager* Separity::UIManager::getInstance() {
+	return static_cast<Separity::UIManager*>(instance());
+}
--- Src/UIEngine/UIManager.h
+++ Src/UIEngine/UIManager.h
@@ -8,6 +8,31 @@ namespace Separity {
 	class UIManager : public Separity::Manager,
 		public Singleton<Separity::UIManager> {
 
+		/// <summary>
+		/// Constructora de la clase
+		/// </summary>
+		inline UIManager();
+		
+		/// <summary>
+		/// Instancia de la clase, necesaria por ser singleton
+		/// </summary>
+		static UIManager* getInstance();
+
+		/// <summary>
+		/// Método para renderizar los elementos de la UI
+		/// </summary>
+		void render();
+
+		/// <summary>
+		/// Método update que hereda de manager
+		/// </summary>
+		virtual void update() override;
+
+		/// <summary>
+		/// Borrado de la clase, hereda de Manager
+		/// </summary>
+		virtual ~UIManager() override;
+
 	};
 }  // namespace Separity
 #endif  //!__UI_MANAGER_H__
