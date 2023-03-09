5cad9b67cd06d6b0041ceed8f5f78f348f87e9fd
Laura <lgomez25@ucm.es>
2023-03-09 17:19:36 +0100

    comienzo a meter imgui en sdl

--- Src/UIEngine/UIManager.cpp
+++ Src/UIEngine/UIManager.cpp
@@ -1,9 +1,19 @@
 #include "UIManager.h"
 
+#include "RenderManager.h"
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+
+#include <SDL.h>
+#include <vector>
+
 template<typename T>
 std::unique_ptr<T> Singleton<T>::_INSTANCE_;
 
-inline Separity::UIManager::UIManager() {}
+inline Separity::UIManager::UIManager() {
+	RenderManager* rM = Separity::RenderManager::getInstance();
+	SDL_Window* window = rM->getSDLWindow();
+}
 
 Separity::UIManager* Separity::UIManager::getInstance() {
 	return static_cast<Separity::UIManager*>(instance());
