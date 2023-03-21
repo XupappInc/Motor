cbfff3056167810680d27a60edc850bae1fab04c
Vicnuel5 <81177633+Vicnuel5@users.noreply.github.com>
2023-03-21 02:31:38 +0100

    distinción de componentes y componentes especiales camara y luz

--- Src/SceneEngine/SceneManager.cpp
+++ Src/SceneEngine/SceneManager.cpp
@@ -24,12 +24,59 @@ bool Separity::SceneManager::loadScene() {
 	rapidjson::Document document;
 	document.ParseStream(stream_wrapper);
 
-	const rapidjson::Value& lightTransformPos =
-	    document["Light"]["Transform"]["pos"];
+	const rapidjson::Value& entities = document["Entities"];
 
-	std::cout << "Light Transform pos: " << lightTransformPos[0].GetDouble()
-	          << ", " << lightTransformPos[1].GetDouble() << ", "
-	          << lightTransformPos[2].GetDouble() << std::endl;
+	rapidjson::Value::ConstMemberIterator entity;
+	for(entity = entities.MemberBegin(); entity != entities.MemberEnd(); ++entity) {
+		// Imprime el nombre del miembro
+		std::cout << entity->name.GetString() << std::endl;
+		
+		rapidjson::Value::ConstMemberIterator component;
+		for(component = entity->value.MemberBegin(); component != entity->value.MemberEnd(); 
+			++component) {
+
+
+			std::string s = component->name.GetString();
+			std::cout << s << std::endl;
+
+			const rapidjson::Value& obj = component->value;
+
+			if(s == "transform") {
+				// Accede a la posición, rotación y escala
+				const rapidjson::Value& pos = obj["pos"];
+				const rapidjson::Value& rot = obj["rot"];
+				const rapidjson::Value& scale = obj["scale"];
+
+				// Imprime la posición, rotación y escala
+				std::cout << "Position: [" << pos[0].GetDouble() << ", "
+				          << pos[1].GetDouble() << ", " << pos[2].GetDouble()
+				          << "]" << std::endl;
+				std::cout << "Rotation: [" << rot[0].GetDouble() << ", "
+				          << rot[1].GetDouble() << ", " << rot[2].GetDouble()
+				          << "]" << std::endl;
+				std::cout << "Scale: [" << scale[0].GetDouble() << ", "
+				          << scale[1].GetDouble() << ", "
+				          << scale[2].GetDouble() << "]" << std::endl;
+			}
+
+			else if(s == "light") {
+				// Accede a la posición, rotación y escala
+				const rapidjson::Value& pos = obj["ambient"];
+
+				// Imprime la posición, rotación y escala
+				std::cout << "Ambiente: [" << pos[0].GetDouble() << ", "
+				          << pos[1].GetDouble() << ", " << pos[2].GetDouble()
+				          << "]" << std::endl;
+			}
+
+			else if (s == "camera") {
+				std::cout << "Soy una camara\n";
+			}
+
+		}
+	}
+
+
 	
 	return false; 
 }
