071f89c1b718f6df2bb5630e6f91bc5e73ca7c66
PaulaMorillasAlonso <pamorill@ucm.es>
2023-04-10 13:40:45 +0200

    añadidos metodos Entity Manager

--- Src/EntityComponent/EntityManager.cpp
+++ Src/EntityComponent/EntityManager.cpp
@@ -1,4 +1,5 @@
 #include "EntityManager.h"
+#include "Entity.h"
 template<typename T>
 std::unique_ptr<T> Singleton<T>::_INSTANCE_;
 
@@ -11,3 +12,7 @@ Separity::EntityManager* Separity::EntityManager::getInstance() { return nullptr
 void Separity::EntityManager::addEntity(Entity* ent) {}
 
 void Separity::EntityManager::deleteEntities() {}
+
+Separity::Entity* Separity::EntityManager::getHandler() { return nullptr; }
+
+Separity::Entity* Separity::EntityManager::getEntByGroup() { return nullptr; }
--- Src/EntityComponent/EntityManager.h
+++ Src/EntityComponent/EntityManager.h
@@ -2,14 +2,14 @@
 #ifndef __ENTITY_MANAGER_H__
 #define __ENTITY_MANAGER_H__
 #include "Manager.h"
+#include <array>
 namespace Separity {
 	class Entity;
 	class EntityManager : public Separity::Manager,
 	                      public Singleton<Separity::EntityManager> {
 		friend Singleton<Separity::EntityManager>;
 
-		public: 
-			public:
+		public:
 		/// <summary>
 		/// Contructor por defecto de la clase
 		/// </summary>
@@ -24,18 +24,23 @@ namespace Separity {
 		/// EntityManager, si ya existiera devuelve dicha instancia, si no
 		/// existiera aún devuelve una nueva instancia
 		/// </summary>
-		/// <returns>Instacia de EntityManager</returns>
+		/// <returns>Instancia de EntityManager</returns>
 		static EntityManager* getInstance();
 		/// <summary>
 		/// Añade una entidad al registro del manager
 		/// </summary>
-		/// <params>Objteo tipo Entity,es la entidad que se quiere añadir al manager</params>
+		/// <params>Objeto tipo Entity,es la entidad que se quiere añadir al manager</params>
 		void addEntity(Entity * ent);
 		/// <summary>
 		/// Borra todas las entidades que hay guardadas
 		/// </summary>
 		void deleteEntities();
 
+		Entity* getHandler();
+		Entity* getEntByGroup();
+
+		std::array<Entity*, Separity::maxHdlrId> hdlrs_;
+		std::array<Entity*, Separity::maxGroupId> entsByGroup_;
 	};
 }  // namespace Separity
 #endif __ENTITY_MANAGER_H__
