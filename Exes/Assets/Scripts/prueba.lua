33b80b8024639e6d805345e73ebf327002a8543d
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2023-04-11 11:13:52 +0200

    cambios a pruebas.lua

--- Exes/Assets/Scripts/prueba.lua
+++ Exes/Assets/Scripts/prueba.lua
@@ -11,11 +11,17 @@ end
 
 function Prueba:update()
     -- print('Se llama al update del script de Lua')
-    -- prueba.entity:getTransform():roll(27);
+    -- prueba.entity:getTransform():roll(27)
 end
 
 function Prueba:onCollisionEnter(other)
-    print('OnCollisionEnter de Sinbad', other)
+    print('OnCollisionEnter de Sinbad')
+
+    if other ~= nil then
+        other:getTransform():roll(90)
+    else
+        print('Other is nil')
+    end
 end
 
 function Prueba:onCollisionExit(other)
@@ -23,7 +29,6 @@ function Prueba:onCollisionExit(other)
 end
 
 function Prueba:onCollisionStay(other)
-
     if collisionStayCalled then
         print('OnCollisionStay de Sinbad')
         collisionStayCalled = false
