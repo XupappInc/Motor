70da2491a0749a925ac7036f106189538d11038c
Sebana02 <sebana@ucm.es>
2023-04-21 10:13:29 +0200

    metodo lookat arreglado

--- Src/SeparityUtils/spyQuaternion.cpp
+++ Src/SeparityUtils/spyQuaternion.cpp
@@ -1,14 +1,16 @@
 #include "spyQuaternion.h"
+
+#include "OgreMatrix3.h"
+#include "OgreQuaternion.h"
 #include "spyMath.h"
 #include "vector.h"
-#include <math.h>
-#include"OgreQuaternion.h"
+
+#include <LinearMath/btQuaternion.h>
 #include <OgreMath.h>
 #include <OgreVector3.h>
-#include <LinearMath/btQuaternion.h>
-Spyutils::spyQuaternion::spyQuaternion():spyQuaternion(0,0,0) {}
+#include <math.h>
+Spyutils::spyQuaternion::spyQuaternion() : spyQuaternion(0, 0, 0) {}
 Spyutils::spyQuaternion::spyQuaternion(float x, float y, float z) {
-
 	double cy = cos(z * Ogre::Math::PI / 180.0 / 2.0);
 	double sy = sin(z * Ogre::Math::PI / 180.0 / 2.0);
 	double cp = cos(y * Ogre::Math::PI / 180.0 / 2.0);
@@ -26,7 +28,7 @@ Spyutils::spyQuaternion::spyQuaternion(float compW, float compX, float compY,
     : x(compX), y(compY), z(compZ), w(compW) {}
 
 Spyutils::spyQuaternion Spyutils::spyQuaternion::Inverse(spyQuaternion q) {
-	  // El cuaternión original
+	// El cuaternión original
 	Spyutils::spyQuaternion q_conjugate = Spyutils::spyQuaternion(
 	    q.w, -q.x, -q.y, -q.z);  // Calculamos el conjugado de q
 	float q_magnitude_squared =
@@ -42,7 +44,7 @@ Spyutils::spyQuaternion Spyutils::spyQuaternion::Conjugate(spyQuaternion q) {
 
 Spyutils::spyQuaternion
 Spyutils::spyQuaternion::difference(Spyutils::spyQuaternion q1,
-                                                  Spyutils::spyQuaternion q2) {
+                                    Spyutils::spyQuaternion q2) {
 	return q1 * Spyutils::spyQuaternion::Conjugate(q2);
 }
 
@@ -67,13 +69,11 @@ float& Spyutils::spyQuaternion::operator[](int index) {
 
 Spyutils::spyQuaternion Spyutils::spyQuaternion::operator/(float const& other) {
 	return Spyutils::spyQuaternion(this->w / other, this->x / other,
-	                               this->y / other,
-	                               this->z / other);
+	                               this->y / other, this->z / other);
 }
 
-
-Spyutils::spyQuaternion Spyutils::spyQuaternion::operator*(spyQuaternion const& q) {
-
+Spyutils::spyQuaternion
+Spyutils::spyQuaternion::operator*(spyQuaternion const& q) {
 	double w_ = w * q.w - x * q.x - y * q.y - z * q.z;
 	double x_ = w * q.x + x * q.w + y * q.z - z * q.y;
 	double y_ = w * q.y - x * q.z + y * q.w + z * q.x;
@@ -94,15 +94,20 @@ Spyutils::Vector3 Spyutils::spyQuaternion::toEulerAngles() {
 }
 
 void Spyutils::spyQuaternion::rotate(float angle, Spyutils::Vector3 axis) {
-
-	Ogre::Quaternion ogreQuaternion(Ogre::Radian(Spyutils::Math::toRadians(angle)), {axis.x, axis.y, axis.z});
-	w = w * ogreQuaternion.w - x * ogreQuaternion.x - y * ogreQuaternion.y - z * ogreQuaternion.z;
-	x = w * ogreQuaternion.x + x * ogreQuaternion.w + y * ogreQuaternion.z - z * ogreQuaternion.y;
-	y = w * ogreQuaternion.y - x * ogreQuaternion.z + y * ogreQuaternion.w + z * ogreQuaternion.x;
-	z = w * ogreQuaternion.z + x * ogreQuaternion.y - y * ogreQuaternion.x + z * ogreQuaternion.w;	
+	Ogre::Quaternion ogreQuaternion(
+	    Ogre::Radian(Spyutils::Math::toRadians(angle)),
+	    {axis.x, axis.y, axis.z});
+	w = w * ogreQuaternion.w - x * ogreQuaternion.x - y * ogreQuaternion.y -
+	    z * ogreQuaternion.z;
+	x = w * ogreQuaternion.x + x * ogreQuaternion.w + y * ogreQuaternion.z -
+	    z * ogreQuaternion.y;
+	y = w * ogreQuaternion.y - x * ogreQuaternion.z + y * ogreQuaternion.w +
+	    z * ogreQuaternion.x;
+	z = w * ogreQuaternion.z + x * ogreQuaternion.y - y * ogreQuaternion.x +
+	    z * ogreQuaternion.w;
 }
 void Spyutils::spyQuaternion::normalizeQuaternion() {
-	double norm = std::sqrt(w *w +x * x + y * y + z * z);
+	double norm = std::sqrt(w * w + x * x + y * y + z * z);
 	w /= norm;
 	x /= norm;
 	y /= norm;
@@ -124,13 +129,11 @@ void Spyutils::spyQuaternion::rotateGlobal(float angle,
 	w = quat.w;
 }
 
-
 Spyutils::Vector3 Spyutils::spyQuaternion::getRotation() {
 	Ogre::Quaternion quat(this->spyQuaterniomToOgre());
 	return Vector3(quat.getPitch().valueDegrees(), quat.getYaw().valueDegrees(),
 	               quat.getRoll().valueDegrees());
 }
-
 
 Ogre::Quaternion Spyutils::spyQuaternion::spyQuaterniomToOgre() const {
 	return Ogre::Quaternion(w, x, y, z);
@@ -138,4 +141,22 @@ Ogre::Quaternion Spyutils::spyQuaternion::spyQuaterniomToOgre() const {
 
 btQuaternion Spyutils::spyQuaternion::spyQuaterniomToBullet() const {
 	return btQuaternion(x, y, z, w);
+}
+
+void Spyutils::spyQuaternion::lookAt(Spyutils::Vector3 target,
+                                     Spyutils::Vector3 position) {
+
+	// Calcular la dirección en la que se debe mirar
+	Spyutils::Vector3 vec = target - position;
+	vec.normalize();
+	Ogre::Vector3 worldY = Ogre::Vector3::UNIT_Y;
+	auto xAxis = worldY.crossProduct({vec.x,vec.y,vec.z});
+	xAxis.normalise();
+	Ogre::Vector3 v = {vec.x, vec.y, vec.z};
+	auto yAxis = v.crossProduct(xAxis);
+	yAxis.normalise();
+	Ogre::Quaternion quat(xAxis, yAxis, v);
+
+	// Convertir la matriz de rotación a un quaternion
+	*this = Spyutils::spyQuaternion(quat.w, quat.x, quat.y, quat.z);
 }
