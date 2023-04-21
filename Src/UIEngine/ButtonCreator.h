cf20f71d126b63218244a43068af07990baa5ac9
damart24 <82331644+damart24@users.noreply.github.com>
2023-04-21 10:35:16 +0200

    Comentados creators de UI

--- Src/UIEngine/ButtonCreator.h
+++ Src/UIEngine/ButtonCreator.h
@@ -10,11 +10,27 @@ namespace Separity {
 
 	class ButtonCreator : public CCreator {
 		public:
+		/// <summary>
+		/// Creador de buttonCreator, vacío de predeterminado
+		/// </summary>
 		ButtonCreator();
 		~ButtonCreator() = default;
 
+		/// <summary>
+		/// Registra en Lua, un tipo de componente en este caso el tipo Button y
+		/// las funciones correspondientes que queramos
+		/// </summary>
 		void registerInLua() override;
 
+		/// <summary>
+		/// Se crea un componente button a partir de un texto escrito en Lua con
+		/// parámetros overlayName que es el nombre que queramos pasarle a dicho
+		/// boton, una posición x, y, y un tamaño, tanto ancho, width
+		///  como alto height, y por último el nombre de la textura que se llama
+		///  texture
+		/// </summary>
+		/// <param name="L"></param>
+		/// <param name="ent"></param>
 		void createComponent(lua_State* L, Separity::Entity* ent) override;
 	};
 };  // namespace Separity
