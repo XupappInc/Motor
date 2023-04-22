db40f679af7278f67a70ca367dc57c8175e9b861
damart24 <82331644+damart24@users.noreply.github.com>
2023-04-22 17:10:46 +0200

    Comentados creators restantes, están todos

--- Src/PhysicsEngine/ColliderCreator.h
+++ Src/PhysicsEngine/ColliderCreator.h
@@ -10,11 +10,24 @@ namespace Separity {
 
 	class ColliderCreator : public CCreator {
 		public:
+		/// <summary>
+		/// Creador de ColliderCreator, vacío de predeterminado
+		/// </summary>
 		ColliderCreator();
 		~ColliderCreator() = default;
 
+		/// <summary>
+		/// Registra en Lua, un tipo de componente en este caso el tipo
+		/// ColliderCreator y las funciones correspondientes que queramos
+		/// </summary>
 		void registerInLua() override {};
 
+		/// <summary>
+		/// Se crea un componente Collider con un array de tamaño 3 indicando el
+		/// ancho, el alto y la profundidad
+		/// </summary>
+		/// <param name="L"></param>
+		/// <param name="ent"></param>
 		void createComponent(lua_State* L, Separity::Entity* ent) override;
 	};
 };  // namespace Separity
--- Src/PhysicsEngine/RigidbodyCreator.h
+++ Src/PhysicsEngine/RigidbodyCreator.h
@@ -10,11 +10,24 @@ namespace Separity {
 
 	class RigidbodyCreator : public CCreator {
 		public:
+		/// <summary>
+		/// Creador de RigidbodyCreator, vacío de predeterminado
+		/// </summary>
 		RigidbodyCreator();
 		~RigidbodyCreator() = default;
 
+		/// <summary>
+		/// Registra en Lua, un tipo de componente en este caso el tipo
+		/// RidigbodyCreator y las funciones correspondientes que queramos
+		/// </summary>
 		void registerInLua() override;
 
+		/// <summary>
+		/// Se crea un componente Rigidbody con un int que dependiendo de que
+		/// número sea, será dinámico o estático
+		/// </summary>
+		/// <param name="L"></param>
+		/// <param name="ent"></param>
 		void createComponent(lua_State* L, Separity::Entity* ent) override;
 	};
 };  // namespace Separity
