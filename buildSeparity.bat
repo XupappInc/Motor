9568179ba7b4b2a65fb9ce19ee7eadefa95cc699
Sergio Baña <sebana@ucm.es>
2023-05-03 22:07:44 +0200

    Update buildSeparity.bat

--- buildSeparity.bat
+++ buildSeparity.bat
@@ -4,3 +4,6 @@ cls
 
 msbuild "Separity.sln" /p:Configuration=Debug /p:Platform=x64
 msbuild "Separity.sln" /p:Configuration=Release /p:Platform=x64
+
+XCOPY /y /s lib\x64\Debug\SeparityExports_d.dll Exes
+XCOPY /y /s lib\x64\Release\SeparityExports.dll Exes
